82930176372aa7572467d51fa5ace7515148f8d2
Ryan Baumgardner <r.baumgardner93@gmail.com>
2018-04-17 08:07:52 -0400

    Fixed routing issue

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. get the text from that input
+    const storeName = this.myInput.value.value;
+
+    // 3. change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please enter a store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store &rarr;</button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
